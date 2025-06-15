e2e187cace1d6cfc188646516acf798f5aa53632
Fayiz <fayiz.islam@gmail.com>
2025-06-16 00:27:03 +0600

    switched to godaddy

--- backend/settings.py
+++ backend/settings.py
@@ -1,27 +1,23 @@
 from pathlib import Path
 import os
-
-
-
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+load_dotenv(BASE_DIR / '.env')
 
-SECRET_KEY = 'django-insecure-w-h!btx5q0^7oziy7-*s5(owe%pemqw4ph8w%5k006l1qu8xcx'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'essentialthreads.netlify.app',   
+    'essentialthreads.netlify.app',
     'essentialthreads-backend.onrender.com',
+    '7536-103-230-62-3.ngrok-free.app',
 ]
-
-
-
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -35,24 +31,23 @@ INSTALLED_APPS = [
     'store',
 ]
 
-STRIPE_PUBLISHABLE_KEY = 'pk_test_51RTSVv4ERU9xOMUAKNWyazOz2R6y09usbqHtYWdz7x5x6aKMuzoCppHUVrPfhOs8TAPrKeLVzzGJ2aslS6zzoFOd00xfhSUT2V'  
-STRIPE_SECRET_KEY = 'sk_test_51RTSVv4ERU9xOMUAp8yi8jKFLs54koDCzJEfOSK6TswyVebsleSjHskmSuinqqp9U20Lz2IuwG8g0U1A19JEXIv500AX59KjfI'      
-STRIPE_WEBHOOK_SECRET ='whsec_IQ1Zi0GkM5OKzBRlpU0bq15ddGT9CztT' 
+STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
 
-PAYPAL_CLIENT_ID='AQGPxvo3Xf0SQcxSSdBiEBLn7N_dKXOWW7JTwVZR-Fz1FMr1yv2i57LmF8YlxWTzGUq55rwORX0g7-vJ'
-PAYPAL_CLIENT_SECRET='EGIvbiQQRMbtqrsLViGQ-YmLk02dXZMyadgBZN-RWtkzyVZgihRL_pfmxI37S2WyyFK2JsSOl-NGyR-N'
+PAYPAL_CLIENT_ID = os.getenv('PAYPAL_CLIENT_ID')
+PAYPAL_CLIENT_SECRET = os.getenv('PAYPAL_CLIENT_SECRET')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -73,17 +68,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'essentialThreads',
-        'USER': 'postgres',
-        'PASSWORD': 'newpassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'your_db_name'),
+        'USER': os.getenv('DB_USER', 'your_db_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'your_db_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
@@ -93,40 +85,25 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://essentialthreads.netlify.app",
+    "https://7536-103-230-62-3.ngrok-free.app",
 ]
-
-
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
 
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
