dbf48ca690215e1633425e9c9096cc8e9bf12cfa
Fayiz <fayiz.islam@gmail.com>
2025-06-16 18:35:39 +0600

    Update requirements.txt after upgrading Stripe

--- store/urls.py
+++ store/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.ProductList.as_view()),
     path('', views.home, name='home'),
-    path('submit-order/', views.submit_order),
+    path('products/', views.ProductList.as_view(), name='product-list'),
+    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+    path('categories/', views.CategoryList.as_view(), name='category-list'),
+    path('submit-order/', views.submit_order, name='submit-order'),
     path('stripe/webhook/', views.stripe_webhook, name='stripe-webhook'),
-    path('paypal-capture', views.paypal_capture),
-    path('success', views.success),
-    path('cancel/', views.cancel),
-    path('categories/', views.CategoryList.as_view()),
-    path("products/<int:pk>/", views.ProductDetail.as_view())
-
+    path('paypal-capture/', views.paypal_capture, name='paypal-capture'),
+    path('success/', views.success, name='success'),
+    path('cancel/', views.cancel, name='cancel'),
 ]
