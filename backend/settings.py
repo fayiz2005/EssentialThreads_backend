304a4b201eef60090d2926c1e4d6895ef92f7352
Fayiz <fayiz.islam@gmail.com>
2025-06-17 22:47:46 +0600

    Add Cloudinary integration to settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -53,11 +53,6 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': CLOUDINARY_API_SECRET,
 }
 
-cloudinary.config(
-    cloud_name=CLOUDINARY_CLOUD_NAME,
-    api_key=CLOUDINARY_API_KEY,
-    api_secret=CLOUDINARY_API_SECRET
-)
 
 
 STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY")
--- store/models.py
+++ store/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-
+from cloudinary.models import CloudinaryField
 
 
 class Category(models.Model):
@@ -18,7 +18,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.URLField(null=True, blank=True)
+    image = CloudinaryField('image') 
     stock = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
